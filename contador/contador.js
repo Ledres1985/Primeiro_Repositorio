94c8c49f0da50ec935845713147047123e26eff1
Ledres1985 <94397954+Ledres1985@users.noreply.github.com>
2023-09-21 10:53:35 -0300

    Add files via upload

---
+++ contador/contador.js
@@ -0,0 +1,35 @@
+const valor = document.querySelector('#valor')
+
+function encremento(){
+
+    valor.innerHTML = parseInt(`${valor.innerText}`) + 1
+
+}
+
+function decremento(){
+
+    valor.innerHTML = parseInt(`${valor.innerText}`) - 1
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const valor = document.querySelector('#valor')
+
+// function incremento(){
+//     valor.innerHTML = parseInt(valor.innerText) + 1
+// }
+
+// function decremento(){
+//     valor.innerHTML = parseInt(valor.innerText) - 1
+// }
